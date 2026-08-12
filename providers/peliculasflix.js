20291ef547f205455c5f987df6012f5655fe3091
manuizoel <88502759+manuizoel@users.noreply.github.com>
2026-08-12 16:34:04 -0400

    Create peliculasflix.js

---
+++ providers/peliculasflix.js
@@ -0,0 +1,14 @@
+async function getStreams(id, type, season, episode) {
+    console.log("Proveedor recibido:", {
+        id,
+        type,
+        season,
+        episode
+    });
+
+    return [];
+}
+
+module.exports = {
+    getStreams
+};
